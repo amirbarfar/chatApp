fdf35377cf82a6a4a9b215a0f726038840f8d8f5
amirbarfar <amirbarfar43@gmail.com>
2025-12-10 21:16:01 +0330

    test

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,12 +3,8 @@ import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const connectionString = process.env.DATABASE_URL;
-
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter: new PrismaPg({ connectionString }),
-  });
+  new PrismaClient({});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
