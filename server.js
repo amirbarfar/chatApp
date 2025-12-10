72360da2a2ef1814fc7ffcd5beb6e761c716f6d4
amirbarfar <amirbarfar43@gmail.com>
2025-12-10 20:05:40 +0330

    start project

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-
 const port = 3000
-
 const wsPort = 3001
 
 app.prepare().then(() => {
