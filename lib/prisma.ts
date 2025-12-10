e7ed2f16f22bdc39eafec6ee9268bee9d6b00ebb
amirbarfar <amirbarfar43@gmail.com>
2025-12-10 21:41:01 +0330

    test

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from '@/prisma/generated/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({});
+  new PrismaClient({ adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }) });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
